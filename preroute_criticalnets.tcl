0822daf413dfaf6135e25d398a51f6e154a31813
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-04-06 12:14:50 +0200

    Renamed critical nets prerouter

--- preroute_criticalnets.tcl
+++ preroute_criticalnets.tcl
@@ -1,3 +1,5 @@
+# Run me before routing, to pre-route and fixate the more difficult nets first
+
 # Get the nets in the top 10 critical paths, assign to $preRoutes
 set preRoutes [get_nets -of [get_timing_paths -max_paths 10]]
 # route $preRoutes first with the smallest possible delay
