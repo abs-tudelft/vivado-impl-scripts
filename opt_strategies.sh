97a9b500b752802f71efe3a89733592b05e04687
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-04-06 12:42:46 +0200

    Added the option to continue a full build or not

--- opt_strategies.sh
+++ opt_strategies.sh
@@ -8,6 +8,9 @@
 #route_design
 #phys_opt_design
 
+# To get a better idea of the influence of this exploration, you can choose to continue all the implementation steps after each opt_design attempt.
+fullbuilds=yes
+
 OPT_STRATS="Explore ExploreArea ExploreWithRemap ExploreSequentialArea"
 
 if [ "$#" -ne 2 ]; then
@@ -19,34 +22,48 @@ fi
 
 starting_checkpoint=$1
 wdir_base=$2
+
 echo "Starting checkpoint: $starting_checkpoint"
 echo "Workspace directory: $wdir_base"
 
+#default strategies for downstream implementation steps
 ps="Explore"
 pos="Explore"
 rs="Explore"
-runs=0
+
 for os in $OPT_STRATS; do
    echo "OPT strategy: $os"
-    runs=$((runs+1))
     wdir=$wdir_base/strategies/$os
     mkdir -p $wdir
     script=$wdir/vivado_script.tcl
+    
+    #Open the starting checkpoint
     echo "open_checkpoint $starting_checkpoint" > $script
+    
+    #Optimize the design
     echo "opt_design -directive $os" >> $script
     echo "write_checkpoint $wdir/opt_design.dcp -force" >> $script
     echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_opt_design.rpt" >> $script
-    echo "place_design -directive $ps" >> $script
-    echo "write_checkpoint $wdir/placed_design.dcp -force" >> $script
-    echo "phys_opt_design -directive $pos" >> $script
-    echo "write_checkpoint $wdir/phys_opt_design.dcp -force" >> $script
-    echo "route_design -directive $rs" >> $script
-    echo "write_checkpoint $wdir/route_design.dcp -force" >> $script
-    echo "phys_opt_design -directive $pos" >> $script
-    echo "write_checkpoint $wdir/opt_routed_design.dcp -force" >> $script
-    echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_opt_routed_design.rpt" >> $script
+    
+    if [ $fullbuilds == "yes" ]; then
+      echo "Performing a full build"
+    
+      #Placement & physical optimization (with default directives)
+      echo "place_design -directive $ps" >> $script
+      echo "write_checkpoint $wdir/placed_design.dcp -force" >> $script
+      echo "phys_opt_design -directive $pos" >> $script
+      echo "write_checkpoint $wdir/phys_opt_design.dcp -force" >> $script
+      echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_phys_opt_design.rpt" >> $script
+      
+      #Routing & physical optimization (with default directives)
+      echo "route_design -directive $rs" >> $script
+      echo "write_checkpoint $wdir/route_design.dcp -force" >> $script
+      echo "phys_opt_design -directive $pos" >> $script
+      echo "write_checkpoint $wdir/opt_routed_design.dcp -force" >> $script
+      echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_opt_routed_design.rpt" >> $script
+    fi
     
     #Run Vivado with the created script
     vivado -quiet -mode batch -source $script -notrace -log $wdir/vivado_build.log -journal $wdir/vivado_build.jou
 done
-#echo "Total number of runs: $runs"
+
--- place_strategies.sh
+++ place_strategies.sh
@@ -10,6 +10,9 @@
 #route_design
 #phys_opt_design
 
+# To get a better idea of the influence of this exploration, you can choose to continue all the implementation steps after each opt_design attempt.
+fullbuilds=yes
+
 PLACE_STRATS="Explore ExtraNetDelay_high ExtraNetDelay_low ExtraPostPlacementOpt ExtraTimingOpt"
 PHYSOPT_STRATS="Explore AggressiveExplore AlternateReplication AggressiveFanoutOpt AlternateFlowWithRetiming"
 ROUTE_STRATS="Explore AggressiveExplore HigherDelayCost AlternateCLBRouting"
@@ -26,6 +29,10 @@ wdir_base=$2
 echo "Starting checkpoint: $starting_checkpoint"
 echo "Workspace directory: $wdir_base"
 
+#default strategies for downstream implementation steps (to get a better idea of the influence of our exploration)
+pos="Explore"
+rs="Explore"
+
 runs=0
 for ps in $PLACE_STRATS; do
   for os in $PHYSOPT_STRATS; do
@@ -36,14 +43,30 @@ for ps in $PLACE_STRATS; do
       wdir=$wdir_base/strategies/$ps/$os
       mkdir -p $wdir
       script=$wdir/vivado_script.tcl
+      
+      #Open the starting checkpoint
       echo "open_checkpoint $starting_checkpoint" > $script
+      
+      #Place the design
       echo "place_design -directive $ps" >> $script
       echo "write_checkpoint $wdir/place_design.dcp -force" >> $script
       echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_place.rpt" >> $script
       
+      #Perform physical optimization
       echo "phys_opt_design -directive $os" >> $script
       echo "write_checkpoint $wdir/phys_opt_design.dcp -force" >> $script
       echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_phys_opt.rpt" >> $script
+      
+      if [ $fullbuilds == "yes" ]; then
+        echo "Performing a full build"
+      
+        #Routing & physical optimization (with default directives)
+        echo "route_design -directive $rs" >> $script
+        echo "write_checkpoint $wdir/route_design.dcp -force" >> $script
+        echo "phys_opt_design -directive $pos" >> $script
+        echo "write_checkpoint $wdir/opt_routed_design.dcp -force" >> $script
+        echo "report_timing_summary -quiet -max_paths 100 -file $wdir/timing_summary_opt_routed_design.rpt" >> $script
+      fi
       
       #Run Vivado with the created script
       vivado -quiet -mode batch -source $script -notrace -log $wdir/vivado_build.log -journal $wdir/vivado_build.jou
