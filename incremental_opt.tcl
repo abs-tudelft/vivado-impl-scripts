f4bcd59f99476451be085f61c827cef008c28846
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-04-06 11:51:38 +0200

    Added a threshold for attempting incremental optimization

--- incremental_opt.tcl
+++ incremental_opt.tcl
@@ -1,4 +1,9 @@
 #incremental optimizations, taken from the Xilinx Design Analysis and Closure Techniques manual (UG906):
+if {[get_property SLACK [get_timing_paths ]] <= -0.5} {
+  puts "The design has significant negative slack. Meeting timing seems highly unlikely and the script will not attempt incremental P&R optimizations."
+  break
+}
+
 for {set i 0} {$i < 3} {incr i} {
   if {[get_property SLACK [get_timing_paths ]] >= 0} {break}; #stop if timing is met
 
